8e910af9753acc1a46f03506ee795ea8bd6d6140
Darien Maillet Valentine <valentinium@gmail.com>
2015-06-03 09:29:44 -0400

    Becoming a thing that does stuff

--- test/test.js
+++ test/test.js
@@ -4,14 +4,37 @@ var testXML  = require('./test-xml.js');
 var Hardcore = require('../lib/node_modules/parser.js');
 var Writable = require('stream').Writable;
 
+function hardcoreTest(t, xml, expected, msg, opts) {
+	var parser = new Hardcore(opts);
+
+	msg = msg || xml;
+	expected = expected || xml;
+
+	parser.on('error', function(err) {
+		t.fail(msg + ' (error: ' + err.message +')');
+	});
+
+	parser.on('document', function(document) {
+		var res = document.toString();
+
+		if (res == expected)
+			t.pass(msg);
+		else
+			t.fail(msg + ' (unexpected output: ' + res + ')');
+	});
+
+	parser.write(xml);
+	parser.end();
+}
+
 tape('Parser Instantiation', function(t) {
 	function invoke() { Hardcore(); }
 	function instantiate() { return new Hardcore(); }
 
-	t.throws(invoke, 'Throws without `new` operator.');
-	t.doesNotThrow(instantiate, 'Does not throw with `new` operator.');
-	t.true(instantiate() instanceof Hardcore, 'Instantiates a new parser.');
-	t.true(instantiate() instanceof Writable, 'Parser is a writable stream.');
+	t.throws(invoke, 'throws without `new` operator');
+	t.doesNotThrow(instantiate, 'does not throw with `new` operator');
+	t.true(instantiate() instanceof Hardcore, 'instantiates a new parser');
+	t.true(instantiate() instanceof Writable, 'parser is a writable stream');
 	t.end();
 });
 
@@ -30,4 +53,64 @@ tape('Parser Method Exposure', function(t) {
 	t.equal(parser._write, _write, '_write method cannot be overwritten');
 
 	t.end();
+});
+
+tape('Strict Document Parsing', function(t) {
+	t.plan(8)
+
+	hardcoreTest(
+		t,
+		'<a></a>',
+		'<a/>',
+		'single empty element'
+	);
+
+	hardcoreTest(
+		t,
+		'<a:b></a:b>',
+		'<a:b/>',
+		'single empty element with namespace'
+	);
+
+	hardcoreTest(
+		t,
+		'<a b="3"></a>',
+		'<a b="3"/>',
+		'single empty element with attribute'
+	);
+
+	hardcoreTest(
+		t,
+		'<a b:c="3"></a>',
+		'<a b:c="3"/>',
+		'single empty element with attribute with namespace'
+	);
+
+	hardcoreTest(
+		t,
+		'<a/>',
+		'<a/>',
+		'single empty element, self-closing'
+	);
+
+	hardcoreTest(
+		t,
+		'<a:b/>',
+		'<a:b/>',
+		'single empty element, self-closing with namespace'
+	);
+
+	hardcoreTest(
+		t,
+		'<a b="3"/>',
+		'<a b="3"/>',
+		'single empty element, self-closing with attribute'
+	);
+
+	hardcoreTest(
+		t,
+		'<a b:c="3"/>',
+		'<a b:c="3"/>',
+		'single empty element, self-closing with attribute with namespace'
+	);
 });
